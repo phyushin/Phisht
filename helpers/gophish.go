ca1852d3ca79286a90d48cc8d362e4c8a36ef5d5
Phyu <phyushin@gmail.com>
2025-04-21 20:04:47 +0100

    Add create sending profile

--- helpers/gophish.go
+++ helpers/gophish.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 )
 
 type options struct {
@@ -17,17 +16,20 @@ type options struct {
 }
 
 type Sending_Profile struct {
-	Profile_name string `json:"profile_name"`
-	From_address string `json:"from_address"`
-	Host         string `json:"host"` // this should be <hostname/ip>:<port>
-	Username     string `json:"username"`
-	Password     string `json:"password"`
+	Profile_name       string   `json:"name"`
+	From_address       string   `json:"from_address"`
+	Host               string   `json:"host"` // this should be <hostname/ip>:<port>
+	Username           string   `json:"username"`
+	Password           string   `json:"password"`
+	Headers            []string `json:"headers"`
+	Interface_type     string   `json:"interface_type"`
+	Ignore_cert_errors bool     `json:"ignore_cert_errors"`
 }
 
 func gophish_api_request(http_method string, request_body []byte, api_url *url.URL) []byte {
 
 	//proxyUrl, err := url.Parse("http://192.168.2.168:8083")
-
+	api_key := "6badb3e54910b3d50c3d99d27b05d7d2629a10af132d642839d11f71addbb049"
 	transCfg := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
 		//Proxy:           http.ProxyURL(proxyUrl),
@@ -35,7 +37,7 @@ func gophish_api_request(http_method string, request_body []byte, api_url *url.U
 
 	httpClient := &http.Client{Transport: transCfg}
 
-	reqUrl := fmt.Sprintf("%s/api/smtp", api_url)
+	reqUrl := fmt.Sprintf("%s/api/smtp/", api_url)
 
 	fmt.Println(reqUrl)
 
@@ -47,17 +49,21 @@ func gophish_api_request(http_method string, request_body []byte, api_url *url.U
 	default:
 	}
 
+	fmt.Println("foo")
+	fmt.Println(req)
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("Authorization", fmt.Sprint(os.Getenv("GOPHISH_API_KEY")))
+	req.Header.Set("Authorization", fmt.Sprint(api_key))
+
 	req.Header.Set("Content-Type", "application/json")
 
 	res, err := httpClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(res.StatusCode)
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
@@ -70,7 +76,6 @@ func gophish_api_request(http_method string, request_body []byte, api_url *url.U
 }
 
 func Create_gophish_sending_profile(profile Sending_Profile) {
-
 	api_url, err := url.Parse("https://localhost:3333")
 	jsonBody, err := json.Marshal(profile)
 	if err != nil {
--- phisht.go
+++ phisht.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"phisht/helpers"
 )
 
@@ -17,10 +19,22 @@ func initialModel() model {
 }
 
 func main() {
+	fmt.Sprint(os.Getenv("GOPHISH_API_KEY"))
 
-	//profile := helpers.Sending_Profile{Profile_name: "foo", From_address: "testing@example.com", Host: "localhost:587", Username: "phyu", Password: "phyu"}
+	headers := []string{}
 
-	//helpers.Create_gophish_sending_profile(profile)
+	profile := helpers.Sending_Profile{
+		Profile_name:       "foo",
+		From_address:       "testing@example.com",
+		Host:               "localhost:587",
+		Username:           "phyu",
+		Password:           "phyu",
+		Headers:            headers,
+		Interface_type:     "SMTP",
+		Ignore_cert_errors: true,
+	}
+
+	helpers.Create_gophish_sending_profile(profile)
 	helpers.Get_gophish_sending_profiles()
 
 }
